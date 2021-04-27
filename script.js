5e237a9166cc1ad3efe7a4fa94892d6b4fa7dd59
gianluca galota <gianluca.galota@gmail.com>
2021-04-27 18:23:24 +0100

    changes js file

--- script.js
+++ script.js
@@ -5,5 +5,4 @@ function handleUpdate() {
   document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
 }
 
-inputs.forEach(input => input.addEventListener('change', handleUpdate));
 inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
